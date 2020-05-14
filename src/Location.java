ced26c6a141f9e5628267148d4f65eb9621f5442
robinsr2 <robinsr2@rose-hulman.edu>
2020-05-14 14:23:26 -0400

    Added PriorityQueue based on location distances (hopefully correct)

--- src/Location.java
+++ src/Location.java
@@ -1,16 +1,20 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-public abstract class Location {
+public abstract class Location implements Comparator<Location>{
 	
 	protected int distance;
 	protected String name;
 	protected int xPos;
 	protected int yPos;
+	protected PriorityQueue<Location> locationQueue;
 	
 	public Location(int x, int y, String name) {
 		this.xPos = x;
 		this.yPos = y;
 		this.name = name;
 		this.distance = 0;
+		this.locationQueue = new PriorityQueue<Location>(10, locationComparator());
 	}
 	
 	public abstract int shortestRoute();
@@ -36,6 +40,32 @@ public abstract class Location {
 	public int getDistance() {
 		return distance;
 	}
+	public void setDIstance(int newDist) {
+		this.distance = newDist;
+	}
+	
+	public int compareTo(Location otherLoc) {
+		if(this.distance > otherLoc.distance) {
+			return 1;
+		}
+		else if(this.distance < otherLoc.distance) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public Comparator<Location> locationComparator(){
+		return new Comparator<Location>() {
+			@Override
+			public int compare(Location loc1, Location loc2) {
+				// TODO Auto-generated method stub
+				return loc1.compareTo(loc2);
+			}
+		};
+	}
+	
 	
 
 }
