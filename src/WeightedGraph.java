48a6137c02a41e968d85501d2c45731af92491b1
Glacie0000 <35363328+Glacie0000@users.noreply.github.com>
2020-05-20 00:21:55 -0700

    fixed a little bit of format

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -10,31 +10,32 @@ public class WeightedGraph {
 	public Vertex[] vertices;
 	public double distance;
 	private int vNum;
-	
-	public WeightedGraph(int vNum){
+
+	public WeightedGraph(int vNum) {
 		this.vNum = vNum;
 		vertices = new Vertex[this.vNum];
 		this.adj = new LinkedList[vNum];
-		for(int i = 0; i < vNum; i++)
+		for (int i = 0; i < vNum; i++)
 			this.adj[i] = new LinkedList<Edge>();
 	}
-	
+
 	public void addEdge(int source, int destination, double weight) {
 		this.adj[source].add(new Edge(source, destination, weight));
 	}
-	
+
 	public void addVertex(int id, double x, double y) {
-		vertices[id] = new Vertex(id,x,y);
+		vertices[id] = new Vertex(id, x, y);
 		System.out.println(id);
 	}
-	
-	private class Vertex implements Comparable<Vertex>{
+
+	private class Vertex implements Comparable<Vertex> {
 		public double x, y;
 		public int id;
-	    public double heuristic;
-	    public double logical;
-	    public double function;
-	    public Vertex lastVertex = null;
+		public double heuristic;
+		public double logical;
+		public double function;
+		public Vertex lastVertex = null;
+
 		public Vertex(int id, double x, double y) {
 			this.x = x;
 			this.y = y;
@@ -43,99 +44,96 @@ public class WeightedGraph {
 			this.heuristic = Double.MAX_VALUE;
 			this.logical = Double.MAX_VALUE;
 		}
+
 		@Override
 		public int compareTo(Vertex vertex) {
 			// TODO Auto-generated method stub
-			if(vertex == null) return -1;
-			if((heuristic+logical) > (vertex.heuristic + vertex.logical)) {
+			if (vertex == null)
+				return -1;
+			if ((heuristic + logical) > (vertex.heuristic + vertex.logical)) {
 				return 1;
-			}else if((heuristic+logical) < (vertex.heuristic + vertex.logical)) {
+			} else if ((heuristic + logical) < (vertex.heuristic + vertex.logical)) {
 				return -1;
 			}
-			
+
 			return 0;
 		}
-		
+
 		@Override
 		public String toString() {
 			return "id: " + id + " heuristic: " + heuristic + " logical: " + logical + "\n";
 		}
-		
+
 	}
-	
+
 	private class Edge {
 		private int source;
 		private int destination;
 		double weight;
-		
+
 		public Edge(int source, int destination, double weight) {
 			this.source = source;
 			this.destination = destination;
 			this.weight = weight;
 		}
-		
+
 	}
-	
+
 	public double distance(Vertex v1, Vertex v2) {
-		return Math.sqrt(Math.pow((v2.x - v1.x), 2.0)+ Math.pow((v2.y - v1.y), 2.0)) * 80000;
+		return Math.sqrt(Math.pow((v2.x - v1.x), 2.0) + Math.pow((v2.y - v1.y), 2.0)) * 80000;
 	}
-	
-	public LinkedList<Integer> shortestPath(Vertex vertex){
-		
+
+	public LinkedList<Integer> shortestPath(Vertex vertex) {
 		LinkedList<Integer> path = new LinkedList<Integer>();
 		path.add(vertex.id);
-		while(vertex.lastVertex != null) {
+		while (vertex.lastVertex != null) {
 			path.add(vertex.lastVertex.id);
 			vertex = vertex.lastVertex;
 		}
-		
+
 		return path;
 	}
-	
+
 	public Vertex Astar(WeightedGraph graph, int source, int destination) {
 		LinkedList<Integer> path = new LinkedList<Integer>();
 		PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>(this.vNum);
-		
+
 		vertices[source].logical = 0;
 		vertices[source].function = 0;
 		queue.add(vertices[source]);
-		
-		
-		while(!queue.isEmpty()) {
+
+		while (!queue.isEmpty()) {
 			System.out.println(queue.toString());
 			Vertex minVertex = queue.poll();
 			path.add(minVertex.id);
-			
-			if(minVertex.id == destination) {
-					// queue.clear();
-					return minVertex;
+
+			if (minVertex.id == destination) {
+				// queue.clear();
+				return minVertex;
 //					break;
 			}
-			for(int i = 0; i < graph.adj[minVertex.id].size(); i++) {
+			for (int i = 0; i < graph.adj[minVertex.id].size(); i++) {
 				Edge edge = graph.adj[minVertex.id].get(i);
 				Vertex vertex = vertices[edge.destination];
-				
-				
-				
-				if((minVertex.logical + edge.weight) < vertex.logical) {
+
+				if ((minVertex.logical + edge.weight) < vertex.logical) {
 					vertex.logical = minVertex.logical + edge.weight;
 					vertex.heuristic = distance(vertex, vertices[destination]);
 					vertex.function = vertex.logical + vertex.heuristic;
 					vertex.lastVertex = minVertex;
-				
-					if(queue.contains(vertex) == false) {
+
+					if (queue.contains(vertex) == false) {
 						queue.add(vertex);
-					}else {
+					} else {
 						queue.remove(vertex);
 						queue.add(vertex);
 					}
-					
+
 				}
 			}
 		}
 		return null;
-		
+
 	}
-	
-	
+
 }
